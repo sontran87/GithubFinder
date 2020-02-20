560ff6e6b0a232ad714656c24626a0ef4bb9e002
sontran87 <transon166@gmail.com>
2020-02-20 10:06:23 +0700

    Show repos to DOM

--- github.js
+++ github.js
@@ -2,14 +2,21 @@ class Github {
   constructor() {
     this.client_id = 'e097b80cd324eba4968b';
     this.client_secret = 'd773b89f8dc366c4b01765bd805bd3bac22582cb';
+    this.repo_count = 5;
+    this.repo_sort = 'created: asc';
   }
   async getUser(user) {
     const profileResponse = await fetch(
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repo_count}&sort=${this.repo_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
     const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
     return {
-      profile
+      profile,
+      repos
     };
   }
 }
--- ui.js
+++ ui.js
@@ -2,6 +2,8 @@ class UI {
   constructor() {
     this.profile = document.getElementById('profile');
   }
+
+  // Show user profile
   showProfile(user) {
     console.log(user);
     this.profile.innerHTML = `
@@ -30,6 +32,29 @@ class UI {
     <h3 class="page-heading mb-3">Lastest Repos</h3>
     <div id="repos"></div>
     `;
+  }
+
+  // Show repos
+  showRepos(repos) {
+    let output = '';
+    repos.forEach(repo => {
+      output += `
+      <div class="card card-body mb-2">
+        <div class="row">
+          <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+          </div>
+          <div class="col-md-6">
+              <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+              <span class="badge badge-secondary">Watcher: ${repo.watchers_count}</span>
+              <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+          </div>
+        </div>
+      </div>
+      `;
+    });
+    // Output repos data
+    document.getElementById('repos').innerHTML = output;
   }
   //Show Alert
   showAlert(message, className) {
